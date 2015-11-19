415ff83611ab64ba10eb4f0cddccc12d1192815d
joakin <joaquin@chimeces.com>
2015-11-19 14:45:26 +0100

    Add a loader component

--- browser/ui/app.js
+++ browser/ui/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from './input.js'
 import Gif from './gif.js'
 import Error from './error.js'
+import Loader from './loader.js'
 
 import './app.less'
 
@@ -9,6 +10,7 @@ export default ({gif, loading, error, onInput}) => (
   <div className='App'>
     <h1>Gimme a gif 👊</h1>
     <Input onSubmit={onInput} loading={loading} />
+    {loading ? <Loader /> : null}
     {error ? <Error {...error} /> : null}
     {gif ? <Gif {...gif} /> : null}
     <p className='giphy'>
