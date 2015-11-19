32176e95d540b49d37ec936f0763d82ef6d0000f
joakin <joaquin@chimeces.com>
2015-11-19 13:34:55 +0100

    Change state management and App component
    Gather more state, make state transitions explicit.
    
    Use stateless component in App, put emoji in title.
    
    Show network errors in the UI.

--- browser/index.js
+++ browser/index.js
@@ -1,18 +1,35 @@
 import React from 'react'
+import { render } from 'react-dom'
 import createApp from './create-app-div.js'
 import App from './ui/app.js'
 import api from './api.js'
 
-let app = createApp('app')
+let appDom = createApp('app')
 
-function render (gif) {
-  React.render(
-    <App
-      onInput={(tag) => api(tag).then(render)}
-      gif={gif}
-    />,
-    app
-  )
+let appState = {
+  gif: null,
+  loading: false,
+  error: { message: 'Ho ho ho' }
 }
 
-render(null)
+const stopLoading = (state) => ({...state, loading: false})
+const newGif = (state, gif) => stopLoading({...state, gif, error: null})
+const error = (state, e) => stopLoading({...state, error: e})
+
+const fetchGif = (tag) =>
+  api(tag).then((gif) => {
+    appState = newGif(appState, gif)
+    renderApp()
+  }).catch((e) => {
+    appState = error(appState, e)
+    renderApp()
+  })
+
+function renderApp () {
+  setTimeout(() => {
+    console.log(appState)
+    render(<App onInput={fetchGif} {...appState} />, appDom)
+  }, 0)
+}
+
+renderApp(null)
--- browser/ui/app.js
+++ browser/ui/app.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import Input from './input.js'
 import Gif from './gif.js'
+import Error from './error.js'
 
 import './app.less'
 
-const App = React.createClass({
-  displayName: 'App',
-  render () {
-    return (
-      <div className='App'>
-        <h1>Gimme a gif</h1>
-        <Input onSubmit={this.props.onInput} />
-        {(this.props.gif ?  <Gif gif={this.props.gif} /> : null)}
-        <p className='giphy'>
-          Powered by <a href='http://giphy.com'>giphy.com</a>
-        </p>
-      </div>
-    )
-  }
-})
-export default App
+export default ({gif, loading, error, onInput}) => (
+  <div className='App'>
+    <h1>Gimme a gif 👊</h1>
+    <Input onSubmit={onInput} loading={loading} />
+    {error ? <Error {...error} /> : null}
+    {gif ? <Gif {...gif} /> : null}
+    <p className='giphy'>
+      Powered by <a href='http://giphy.com'>giphy.com</a>
+    </p>
+  </div>
+)
