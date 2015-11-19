4a603cd796e485a381048ff052d02549cc15f2b4
joakin <joaquin@chimeces.com>
2015-11-19 15:00:21 +0100

    Add Image loader while loading the gif image.

--- browser/ui/gif.js
+++ browser/ui/gif.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Error from './error.js'
+import ImageLoader from 'react-imageloader'
+import Loader from './loader.js'
 
 import './gif.less'
 
@@ -7,7 +9,12 @@ export default (gif) => (
   gif.data.image_url
   ? <div className='Gif'>
       <input type='text' readOnly value={gif.data.image_url}/>
-      <img key={gif.data.image_url} src={gif.data.image_url} />
+      <ImageLoader
+        src={gif.data.image_url}
+        wrapper={React.DOM.div}
+        preloader={() => <Loader /> }>
+        Image load failed!
+      </ImageLoader>
     </div>
   : <Error message='No gif found with that search!'/>
 )
